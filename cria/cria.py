44e29629df84a12ceb0aaddf96b241213305ac71
zipped <100anonyo@gmail.com>
2024-04-21 04:49:21 +0000

    model match

--- cria/cria.py
+++ cria/cria.py
@@ -9,12 +9,48 @@ import time
 from ollama._client import Client as OllamaClient
 import ollama
 
-from httpx import ConnectError
+from httpx import ConnectError, ReadError
 
 DEFAULT_MODEL = "llama3:8b"
 DEFAULT_MESSAGE_HISTORY = [
     {"role": "system", "content": "You are a helpful AI assistant."}
 ]
+
+
+def check_models(model, silence_output):
+    model_list = ollama.list().get("models", [])
+    for m in model_list:
+        m_name = m.get("name", "")
+        if m_name == model:
+            return model
+        if model in m_name:
+            m_without_version = next(iter(m_name.split(":")), "")
+            if model == m_without_version:
+                if not silence_output:
+                    print(f"LLM model found, running {m_name}...")
+                return m_name
+            else:
+                if not silence_output:
+                    print(f"LLM partial match found, running {m_name}...")
+                return m_name
+    model_match = next(
+        (True if m.get("name") == model else False for m in model_list), False
+    )
+    if model_match:
+        return
+
+    if not silence_output:
+        print(f"LLM model not found, downloading '{model}'...")
+
+    try:
+        ollama.pull(model)
+        if not silence_output:
+            print(f"'{model}' downloaded, starting processes.")
+    except Exception as e:
+        print(e)
+        raise ValueError(
+            "Invalid model passed. See the model library here: https://ollama.com/library"
+        )
 
 
 class Client(OllamaClient):
@@ -87,14 +123,21 @@ class Client(OllamaClient):
         ]
 
 
-class Model(Client, ContextDecorator):
+class Cria(Client):
     def __init__(
         self,
         model: Optional[str] = DEFAULT_MODEL,
-        capture_output: Optional[bool] = False,
         run_subprocess: Optional[bool] = False,
-        close_on_exit: Optional[bool] = False,
+        run_llm: Optional[bool] = True,
+        capture_output: Optional[bool] = False,
+        silence_output: Optional[bool] = False,
+        close_on_exit: Optional[bool] = True,
     ) -> None:
+        self.run_subprocess = run_subprocess
+        self.capture_output = capture_output
+        self.silence_output = silence_output
+        self.close_on_exit = close_on_exit
+
         process_name = "ollama"
         ollama_pids = []
         ollama_running = False
@@ -118,6 +161,11 @@ class Model(Client, ContextDecorator):
                 ollama_process.kill()
             ollama_running = False
 
+        try:
+            ollama.list()
+        except (ConnectError, ReadError):
+            ollama_running = False
+
         if not ollama_running:
             ollama_stdout = subprocess.PIPE if capture_output else subprocess.DEVNULL
             ollama_stderr = subprocess.PIPE if capture_output else subprocess.DEVNULL
@@ -129,29 +177,86 @@ class Model(Client, ContextDecorator):
                 try:
                     ollama.list()
                     break
-                except ConnectError:
-                    time.sleep(1)
+                except (ConnectError, ReadError):
+                    time.sleep(2)
                     retries -= 1
         else:
             self.ollama_subrprocess = None
 
+        model = check_models(model, silence_output)
         self.model = model
+
+        if run_llm:
+            self.llm = subprocess.Popen(
+                ["ollama", "run", model],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+
+        if close_on_exit and self.ollama_subrprocess:
+            atexit.register(lambda: self.ollama_subrprocess.kill())
+
+        if close_on_exit and run_llm:
+            atexit.register(lambda: self.llm.kill())
+
+    def output(self):
+        ollama_subprocess = self.ollama_subrprocess
+        if not ollama_subprocess:
+            raise ValueError(
+                "Ollama is not running as a subprocess, you must pass run_subprocess as True to capture output."
+            )
+        if not self.capture_output:
+            raise ValueError(
+                "You must pass in capture_ouput as True to capture output."
+            )
+
+        return iter(lambda: ollama_subprocess.stdout.read(1), "")
+
+    def close(self):
+        llm = self.llm
+        llm.kill()
+
+
+class Model(Cria, ContextDecorator):
+    def __init__(
+        self,
+        model: Optional[str] = DEFAULT_MODEL,
+        capture_output: Optional[bool] = False,
+        silence_output: Optional[bool] = False,
+        close_on_exit: Optional[bool] = True,
+    ) -> None:
+        super().__init__(
+            model=model,
+            capture_output=capture_output,
+            run_subprocess=True if capture_output else False,
+            run_llm=False,
+            close_on_exit=close_on_exit,
+        )
+
+        self.capture_output = capture_output
+        self.silence_output = silence_output
+        self.close_on_exit = close_on_exit
+
+        llm_stdout = subprocess.PIPE if capture_output else subprocess.DEVNULL
+        llm_stderr = subprocess.PIPE if capture_output else subprocess.DEVNULL
         self.llm = subprocess.Popen(
-            ["ollama", "run", model],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
+            ["ollama", "run", model], stdout=llm_stdout, stderr=llm_stderr
         )
 
         if close_on_exit:
             atexit.register(lambda: self.llm.kill())
 
+    def capture_output(self):
+        if not self.capture_output:
+            raise ValueError(
+                "You must pass in capture_ouput as True to capture output."
+            )
+
+        return iter(lambda: self.llm.stdout.read(1), "")
+
     def __enter__(self):
         return self
 
     def __exit__(self, *exc):
-        llm = self.llm
-        llm.kill()
-
-    def close(self):
         llm = self.llm
         llm.kill()
