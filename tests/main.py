e21ba31e0456887c2448568965456b13d98ab0ab
zipped <100anonyo@gmail.com>
2024-04-22 00:37:35 +0000

    message history

--- tests/main.py
+++ tests/main.py
@@ -6,5 +6,17 @@ sys.path.append("../")
 
 from src import cria
 
-ai = cria.Cria(capture_output=True)
-output = ai.
+ai = cria.Cria()
+
+
+context = "Our AI system employed a hybrid approach combining reinforcement learning and generative adversarial networks (GANs) to optimize the decision-making..."
+messages = [
+    {"role": "system", "content": "You are a technical documentation writer"},
+    {"role": "user", "content": context},
+]
+
+prompt = "Write some documentation using the text I gave you."
+for chunk in ai.chat(messages=messages, prompt=prompt):
+    print(
+        chunk, end=""
+    )  # AI System Optimization: Hybrid Approach Combining Reinforcement Learning and...
